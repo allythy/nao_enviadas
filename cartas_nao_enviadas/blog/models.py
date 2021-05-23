191d1ef51e1703bcbea2816eb627cbd312db4aeb
Allythy <allythy@protonmail.com>
2021-05-22 22:42:38 -0300

    feat: Changing the color of the text depending on the selected luminance

--- cartas_nao_enviadas/blog/models.py
+++ cartas_nao_enviadas/blog/models.py
@@ -1,20 +1,23 @@
-from datetime import date, datetime
+from django.db import models
+from colour import Color
 from colorfield.fields import ColorField
 
-from django.contrib.auth.models import User
-from django.db import models
-from django.urls import reverse
 
 class Letter(models.Model):
-    
     name = models.CharField(max_length=255)
     message = models.TextField(max_length=250)
     letter_date = models.DateField(auto_now_add=True)
-    color = ColorField(default='#FFF')
+    color = ColorField(default="#FFF")
 
+    @property
+    def text_color(self):
+        color = Color(self.color)
+        if color.luminance > 0.5:
+            text_color = "black"
+        else:
+            text_color = "white"
+
+        return Color(text_color).get_hex()
 
     def __str__(self):
         return f"{self.name}"
-
-    def get_absolute_url(self):
-        return reverse('home')
--- cartas_nao_enviadas/blog/views.py
+++ cartas_nao_enviadas/blog/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Q
 from django.views.generic import CreateView, DeleteView, ListView
+from django.urls import reverse_lazy
 
 from .forms import LetterForm
 from .models import Letter
@@ -13,25 +14,23 @@ class HomeView(ListView):
 
     def get_queryset(self):
         qs = super().get_queryset()
-        query = self.request.GET.get('busca')
+        query = self.request.GET.get("busca")
         if query:
-            qs = qs.filter(Q(name__contains=query)|Q(message__contains=query))
+            qs = qs.filter(Q(name__contains=query) | Q(message__contains=query))
         return qs
 
     def get_context_data(self):
         context = super().get_context_data()
-        return {**context, 'busca': self.request.GET.get('busca', '')}
-    
-    # queryset = Letter.objects.filter(name__contains='allythy')
+        return {**context, "busca": self.request.GET.get("busca", "")}
 
 
 class LetterDetailView(DeleteView):
     model = Letter
-    template_name = 'letter_details.html'
+    template_name = "letter_details.html"
 
 
 class AddLetterView(CreateView):
     model = Letter
     form_class = LetterForm
-    template_name = 'add_letter.html'
-    # fields = '__all__'
+    template_name = "add_letter.html"
+    success_url = reverse_lazy("home")
